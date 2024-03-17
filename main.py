54b2494ef47ec4d159e0866391e48352c687428e
Rostislav384 <kvasikggdelaet@gmail.com>
2024-03-17 16:04:04 +0300

    создала функцию минуса

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 def menu():
     answer = input('Выберите действие:\n'
                    '+ - сложение\n'
@@ -12,23 +13,25 @@ def menu():
     if answer == '+':
         a = int(input('Введите первое число:'))
         b = int(input('Введите второре число:'))
-        print(f'Ответ: {a+b}')
-    elif answer =='-':
+        print(f'Ответ: {a + b}')
+    elif answer == '-':
+        num1 = int(input('введите 1 число:'))
+        num2 = int(input('введите 2 число:'))
+        print(f'ответ: {num1 - num2}')
+    elif answer == '*':
         pass
-    elif answer =='*':
+    elif answer == '/':
         pass
-    elif answer =='/':
+    elif answer == '**':
         pass
-    elif answer =='**':
+    elif answer == '%':
         pass
-    elif answer =='%':
+    elif answer == '//':
         pass
-    elif answer =='//':
+    elif answer == 'sin':
         pass
-    elif answer =='sin':
+    elif answer == 'cos':
         pass
-    elif answer =='cos':
-        pass
+
 
 menu()
-

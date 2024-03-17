8fc7d9f1b73cc246d1c8ce9530999a1bae95c003
Akyn <akinbayram56@gmail.com>
2024-03-17 16:09:35 +0300

    ALL fix

--- main.py
+++ main.py
@@ -1,33 +1,3 @@
-def menu():
-    answer = input('Выберите действие:\n'
-                   '+ - сложение\n'
-                   '- - вычитание\n'
-                   '* - умножение\n'
-                   '/ - деление\n'
-                   '** - возведение в степень\n'
-                   '% - остаток от деления\n'
-                   '// - целочисленное деление\n'
-                   'sin - синус\n'
-                   'cos - косинус:')
-    if answer == '+':
-        plus()
-    elif answer =='-':
-        minus()
-    elif answer =='*':
-        multiple()
-    elif answer =='/':
-        divide()
-    elif answer =='**':
-        stepen()
-    elif answer =='%':
-        percent()
-    elif answer =='//':
-        fullch()
-    elif answer =='sin':
-        pass
-    elif answer =='cos':
-        pass
-
 def minus():
     a = int(input('Введите первое число:'))
     b = int(input('Введите второре число:'))
@@ -62,6 +32,40 @@ def fullch():
     a = int(input('Введите первое число:'))
     b = int(input('Введите второре число:'))
     print(f'Ответ: {a // b}')
+
+
+
+def menu():
+    answer = input('Выберите действие:\n'
+                   '+ - сложение\n'
+                   '- - вычитание\n'
+                   '* - умножение\n'
+                   '/ - деление\n'
+                   '** - возведение в степень\n'
+                   '% - остаток от деления\n'
+                   '// - целочисленное деление\n'
+                   'sin - синус\n'
+                   'cos - косинус:')
+    if answer == '+':
+        plus()
+    elif answer =='-':
+        minus()
+    elif answer =='*':
+        multiple()
+    elif answer =='/':
+        divide()
+    elif answer =='**':
+        stepen()
+    elif answer =='%':
+        percent()
+    elif answer =='//':
+        fullch()
+    elif answer =='sin':
+        pass
+    elif answer =='cos':
+        pass
+
+
 
 
 
